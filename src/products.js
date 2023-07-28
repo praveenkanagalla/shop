c8c387761649d127ee6d1b58e4bfbe9d5135b0e3
Praveen Kumar Kanagalla <praveenkumarkanagalla@gmail.com>
2023-07-28 22:19:17 +0530

    Added Changes

--- src/products.js
+++ src/products.js
@@ -1,59 +1,59 @@
 import product1 from "./assets/101.png";
-import product2 from "./assets/1.jpeg";
-import product3 from "./assets/1.jpeg";
-import product4 from "./assets/1.jpeg";
-import product5 from "./assets/1.jpeg";
-import product6 from "./assets/1.jpeg";
-import product7 from "./assets/1.jpeg";
+import product2 from "./assets/102.jpg";
+import product3 from "./assets/103.jpg";
+import product4 from "./assets/104.jpg";
+import product5 from "./assets/105.jpg";
+import product6 from "./assets/106.jpg";
+import product7 from "./assets/107.jpg";
 import product8 from "./assets/1.jpeg";
 
 export const PRODUCTS = [
   {
     id: 1,
     productName: "IPhone",
-    price: 79999.00,
+    price: 79999.0,
     productImage: product1,
   },
   {
     id: 2,
-    productName: "Macbook Pro 2022 (M1)",
-    price: 1999.0,
+    productName: "OnePlus Nord 3 5G",
+    price: 33999.0,
     productImage: product2,
   },
   {
     id: 3,
-    productName: "Cannon M50 Camera",
-    price: 699.0,
+    productName: "realme narzo N53",
+    price: 10999.0,
     productImage: product3,
   },
   {
     id: 4,
-    productName: "WLS Van Gogh Denim Jacket",
-    price: 228.0,
+    productName: "Oppo A78 5G",
+    price: 18999.0,
     productImage: product4,
   },
   {
     id: 5,
-    productName: "LED Light Strips",
-    price: 19.99,
+    productName: "Redmi 12C",
+    price: 8499.0,
     productImage: product5,
   },
   {
     id: 6,
-    productName: "SPECTRUM LS TEE",
-    price: 68.0,
+    productName: "Itel A60s",
+    price: 6499.0,
     productImage: product6,
   },
   {
     id: 7,
-    productName: "AUTO SERVICE SHIRT by GOLF WANG",
-    price: 120.0,
+    productName: "OnePlus Nord 3 5G",
+    price: 33999.0,
     productImage: product7,
   },
   {
     id: 8,
-    productName: "DON'T TRIP UNSTRUCTURED HAT",
-    price: 40.0,
+    productName: "Redmi Note 11",
+    price: 13499.0,
     productImage: product8,
   },
 ];
